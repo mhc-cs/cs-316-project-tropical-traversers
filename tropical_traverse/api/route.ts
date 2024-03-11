99bc90de7ca616a91e2111e83cf01b52961b659e
Carissa Willis <willi44c@mthoyloke.edu>
2024-03-11 12:34:46 -0400

    Added MongoDB to backend

---
+++ tropical_traverse/api/route.ts
@@ -0,0 +1,8 @@
+import {NextResponse} from 'next/server';
+import HelloModel from './db';
+
+// Gets all language data from the DB
+export async function GET(request: Request) {
+  const res = await HelloModel.find({});
+  return NextResponse.json(res || {});
+}
