406b644e9f36d71c15738171c03025f773d8bcb6
Carissa Willis <willi44c@mtholyoke.edu>
2024-04-19 23:15:06 -0400

    Added account making required fields

--- tropical_traverse/src/app/driverSignup/page.tsx
+++ tropical_traverse/src/app/driverSignup/page.tsx
@@ -1,11 +1,13 @@
 "use client"; //In order to define a use client <--- very important otherwise the page won't load
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from "axios";
 import "./driverSignup.css";
 import router, { useRouter } from "next/navigation";
 
 const CreateAcc: React.FC = () => {
+
+  const formRef = useRef<HTMLFormElement>(null);
 
   //account constants for data
   const [input, setInput] = useState({
@@ -55,26 +57,14 @@ const CreateAcc: React.FC = () => {
     };
 
     reader.readAsDataURL(file);
-
-    // try{
-    //     const pic = event.target.files[0];
-    //     setInput(prevInput => ({
-    //       ...prevInput,
-    //       [question]: pic
-    //     }));
-
-        // setFilename(pic.name);
-        //const objectURL = URL.createObjectURL(file);
-        
-    // }
-    // catch(error){
-    //     console.error('Error creating object URL:', error);
-    // }
 }
 
+//Paishes Kingston, montego Bay, negril, falmouth, ocho rios, portmore
   //click event handler
-  function handleClick(event:React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
+  function handleClick(event?:React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if (event){
+      event.preventDefault();
+    }
     const newAcc = {
       nameF: input.nameF,
       nameL: input.nameL,
@@ -88,8 +78,54 @@ const CreateAcc: React.FC = () => {
       type: 'driver'
     }
 
-
     axios.post('http://localhost:5000/userAccounts', newAcc)
+    .then(() => {
+      // Clear input fields after successful submission
+      setInput({
+        nameF: '',
+        nameL:'',
+        username: '',
+        email: '',
+        password: '',
+        vehicle: '',
+        license: '',
+        stateID: '',
+        licenseImg: '',
+        type: ''
+      });
+      setFile({ image1: null, image2: null });
+      setFilename({ image1: 'Choose file', image2: 'Choose file' });
+      setPreviewURL({ image1: null, image2: null });
+
+      if (formRef.current){
+        formRef.current.reset();
+      }
+    })
+    .catch((error) => {
+      console.error('Error submitting form:', error);
+
+    });
+  }
+
+  //Checks for empty fields
+  function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.currentTarget;
+
+    const inputs = form.querySelectorAll<HTMLInputElement>('input[required]');
+    let isValid = true;
+    inputs.forEach((input: HTMLInputElement) => {
+      if (!input.value){
+        isValid = false;
+        input.reportValidity();
+      }
+    });
+    if(isValid) {
+      // form.submit();
+      handleClick();
+    }else {
+      alert('Information Incomplete - Please try again.')
+    }
   }
   
 
@@ -100,22 +136,22 @@ console.log('Preview URL:', previewURL);
   return(
     <>
     <div className ='container'>
-        <form>
+        <form ref={formRef} onSubmit={handleFormSubmit}>
             <h1>Create Account</h1>
-            <input onChange={handleChange} name="nameF" value = {input.nameF} className="form-control" placeholder="First Name"></input>
-            <input onChange={handleChange} name="nameL" value = {input.nameL} className="form-control" placeholder="Last Name"></input>
-            <input onChange={handleChange} name="username" value = {input.username} className="form-control" placeholder="Username"></input>
-            <input onChange={handleChange} name="email" value = {input.email} className="form-control" placeholder="Email"></input>
-            <input onChange={handleChange} name="password" value = {input.password}  className="form-control" placeholder="Password" type='password'></input>
-            <input onChange={handleChange} name="vehicle" value = {input.vehicle}  className="form-control" placeholder="Vehicle Model"></input>
-            <input onChange={handleChange} name="license" value = {input.license}  className="form-control" placeholder="License Number"></input>
-            <label htmlFor="stateID">State ID:</label>
-            <input onChange={(e) => handlePics(e, 'stateID')} name="stateID"  className="form-control" type='file'></input>
+            <input onChange={handleChange} name="nameF" value = {input.nameF} className="form-control" placeholder="First Name*" required></input>
+            <input onChange={handleChange} name="nameL" value = {input.nameL} className="form-control" placeholder="Last Name*" required></input>
+            <input onChange={handleChange} name="username" value = {input.username} className="form-control" placeholder="Username*" required></input>
+            <input onChange={handleChange} name="email" value = {input.email} className="form-control" placeholder="Email*" required></input>
+            <input onChange={handleChange} name="password" value = {input.password}  className="form-control" placeholder="Password*" type='password' required></input>
+            <input onChange={handleChange} name="vehicle" value = {input.vehicle}  className="form-control" placeholder="Vehicle Model*" required></input>
+            <input onChange={handleChange} name="license" value = {input.license}  className="form-control" placeholder="License Number*" required></input>
+            <label htmlFor="stateID">State ID*:</label>
+            <input onChange={(e) => handlePics(e, 'stateID')} name="stateID"  className="form-control" type='file' required></input>
             {/* {previewURL && (<img src={previewURL} alt="Selected" style={{ maxWidth: '100%', maxHeight: '200px' }}/>)} */}
-            <label htmlFor="licenseImg">License:</label>
-            <input onChange={(e) => handlePics(e, 'licenseImg')} name="licenseImg"  className="form-control" type='file'></input>
+            <label htmlFor="licenseImg">License*:</label>
+            <input onChange={(e) => handlePics(e, 'licenseImg')} name="licenseImg"  className="form-control" type='file' required></input>
             {/* {previewURL && (<img src={previewURL} alt="Selected" style={{ maxWidth: '100%', maxHeight: '200px' }}/>)} */}
-            <button onClick={handleClick}className="btn btn-lg btn-info">Create Account</button>
+            <button className="btn btn-lg btn-info">Create Account</button>
             <span>Already have an account? <button onClick={(event) => { event.preventDefault(); router.push(url); }}><b>Login here</b></button> </span>
         </form>
     </div>
